32f2bd732b1b93d8acffcbe66c97c3525ace862d
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-12-07 09:10:11 -0400

    GG

--- Presentation1/res/data/slide29.js
+++
@@ -1,1 +0,0 @@
-(function(){var loadHandler=window['sl_{4BF57FEA-2BD3-44B5-88E0-D614A2BB2AE2}'];loadHandler&&loadHandler(28, '<div id="spr0_d3682f9"><div id="spr1_d3682f9" class="kern slide"><img id="img0_d3682f9" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_d3682f9" class="kern slide"></div></div>', '{"s":[]}');})();
